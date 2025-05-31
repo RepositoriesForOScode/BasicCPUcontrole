65d5ef00e661b47413c338e11bf65975c6f2b59e
johnnycodingj <157506001+johnnycodingj@users.noreply.github.com>
2025-05-31 10:38:56 -0400

    Add files via upload
    This is a small program used to controle the cpu by making it wait to output the string instead of doing it as soon as we run the program, every 10 seconds its supposed to print hello

---
+++ CPU.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class CPU {
+
+    public static void CPUpause(String[] args) { // String[] args is a list of strings
+        args = UserInput(args);
+        System.err.println(args.length);
+        if (args.length != 1) { // if the length is 0 or greater than 1 
+            System.err.println("Usage: java CPU <string>"); // print error message
+            System.exit(1); // terminate program
+        }
+
+        String str = args[0]; // the one string element the the user provides in command line
+
+        while (true) {
+            // choosing 10 seconds for testing perposes, making the cpu wait 10 seconds before printing string
+            spin(10);  // tells the cpu to wait, halt oporations on the proggram
+            System.out.println(str); // print out the string from the users command line entries
+        }
+    }
+
+    // Simulates CPU spin for the given number of seconds
+    public static void spin(int seconds) {
+        long start = System.nanoTime(); // gets the current time in nanoseconds
+        long duration = seconds * 1_000_000_000L; // convert to nanoseconds to get durration in nanoseconds
+
+        while ((System.nanoTime() - start) < duration) { // the diffrence between start time and current time 
+            // has to be less than the durration, for example if start time is 50 and current time
+            // is 60 and the duration is 10 sec then than the while loop will die.
+        }
+    }
+
+    public static String[] UserInput(String[] args) {
+        Scanner scanner = new Scanner(System.in); // create a scanner object to read user input
+        System.out.print("Enter a string: "); // prompt the user for input
+        String input = scanner.nextLine(); // read the input from the use
+        args = Arrays.copyOf(args, args.length + 1); // add length to the the array
+        args[args.length - 1] = input; // add user input to the array
+        scanner.close(); // close user input
+        return args; // return new array
+    }
+
+}
